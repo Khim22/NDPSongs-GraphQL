21f034c2b61b04303b3063c43fff6e934cf0eaa1
Khim22 <khimyang22@gmail.com>
2019-08-09 23:14:02 +0800

    Updates

---
+++ precache-manifest.e49f8b06268030b2f29b68b7cd950df8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e62a3d00181abad6340ff2aecb5740c",
+    "url": "/NDPSongs-GraphQL/index.html"
+  },
+  {
+    "revision": "50a1967e45ee2f869342",
+    "url": "/NDPSongs-GraphQL/static/css/main.fe3db0c7.chunk.css"
+  },
+  {
+    "revision": "bf045459a99f7463d877",
+    "url": "/NDPSongs-GraphQL/static/js/2.dceaa358.chunk.js"
+  },
+  {
+    "revision": "50a1967e45ee2f869342",
+    "url": "/NDPSongs-GraphQL/static/js/main.01b513e2.chunk.js"
+  },
+  {
+    "revision": "43835900a7488242c31c",
+    "url": "/NDPSongs-GraphQL/static/js/runtime~main.54fcdee8.js"
+  }
+]);
